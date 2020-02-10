d15869012a03b29a97a4624ddcafb43b2f683194
Erik Veld <eveld@hashicorp.com>
2020-02-10 16:45:03 +0100

    v0.3.9

--- dist/Terminal.js
+++ dist/Terminal.js
@@ -21,6 +21,7 @@ const Term = ({
       fontFamily: 'Hack, Droid Sans Mono, Monospace',
       fontSize: 14,
       lineHeight: 1.1,
+      cursorBlink: true,
       theme: {
         "background": "#011627",
         "black": "#011627",
@@ -64,9 +65,10 @@ const Terminal = ({
   workdir = '/',
   user = 'root',
   shell = '/bin/sh',
-  placeholder = 'Click to open terminal'
+  placeholder = 'click to expand terminal',
+  expanded = false
 }) => {
-  const [show, setShow] = useState(false);
+  const [show, setShow] = useState(expanded);
   return React.createElement(React.Fragment, null, show ? React.createElement(Term, {
     target: target,
     workdir: workdir,
@@ -75,7 +77,16 @@ const Terminal = ({
   }) : React.createElement("div", {
     className: "placeholder",
     onClick: () => setShow(true)
-  }, placeholder));
+  }, React.createElement("svg", {
+    className: "icon",
+    "aria-hidden": "true",
+    focusable: "false",
+    viewBox: "0 0 640 512"
+  }, React.createElement("path", {
+    fill: "#ADDB67",
+    d: "M257.981 272.971L63.638 467.314c-9.373 9.373-24.569 9.373-33.941 0L7.029 444.647c-9.357-9.357-9.375-24.522-.04-33.901L161.011 256 6.99 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L257.981 239.03c9.373 9.372 9.373 24.568 0 33.941zM640 456v-32c0-13.255-10.745-24-24-24H312c-13.255 0-24 10.745-24 24v32c0 13.255 10.745 24 24 24h304c13.255 0 24-10.745 24-24z",
+    class: ""
+  })), "                ", placeholder));
 };
 
 export default Terminal;
